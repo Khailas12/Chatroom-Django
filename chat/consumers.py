992f592da794cb90ac93f8f9859f09097a402596
Khailas R <khailas303@gmail.com>
2021-12-22 23:00:14 +0530

    Websocket update

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,4 +2,10 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
-    pass
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        
+        await (
+        
+        )
--- main/asgi.py
+++ main/asgi.py
@@ -1,12 +1,32 @@
 import os
 
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
+application = get_asgi_application()
+
+
+"""
+import os
 import django
-from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from chat.consumers import ChatRoomConsumer
+from django.conf.urls import url
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
-  "http": AsgiHandler(),
+    "http": get_asgi_application(),
+
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            url(r'ws/chat/(?P<room_name>\w+)/$', ChatRoomConsumer.as_asgi()),
+        ])
+    ),
 })
+"""
